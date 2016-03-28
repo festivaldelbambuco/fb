6f120cc7f1a3c67997fb8d597c755a9ddd018dfd
festivaldelbambuco <festivaldelbambucoapp@gmail.com>
2016-03-28 02:58:12 -0500

    Added files via upload

---
+++ credentials.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Facebook Access
+* Author: evilnapsis
+**/
+
+$login_url = "http://evilnapsis.com/stuffs/fb-access/callback.php"; // ejemplo de callback
+$app_id = "INSERTA_TU_APP_ID";
+$app_secret = "INSERTA_TU_APP_SECRET";
+
+?>
